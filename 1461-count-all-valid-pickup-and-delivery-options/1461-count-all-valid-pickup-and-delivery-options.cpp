32d9356984e39460ab2fa6c19a149b63308796a9
Subhash D <subhash613d@gmail.com>
2023-09-10 20:23:49 +0530

    Time: 0 ms (100%), Space: 5.9 MB (71.3%) - LeetHub

---
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+        int countOrders(int n)
+        {
+            long long MODULO = 1e9 + 7, ans = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int spaceNum = (i - 1) *2 + 1;
+                ans *= spaceNum *(spaceNum + 1) / 2;
+                ans %= MODULO;
+            }
+            return (int) ans;
+        }
+};
