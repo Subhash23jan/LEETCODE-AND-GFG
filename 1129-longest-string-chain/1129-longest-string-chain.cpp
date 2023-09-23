a915b786aa67d3e38a0192bb2280bae88a66f2e3
Subhash D <subhash613d@gmail.com>
2023-09-23 14:23:36 +0530

    Time: 246 ms (31.58%), Space: 59.7 MB (31.33%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestStrChain(vector<string>& words) {
         
-    std::sort(words.begin(), words.end(), [](const std::string& first, const std::string& second)
+    std::sort(words.begin(), words.end(), [](string first, string second)
 	{
         return first.size() < second.size();
 	});
@@ -15,7 +15,7 @@ public:
             int longest =0;
             for(int i = 0;i<word.length();i++)
             {
-                string sub = word.substr(0,i) + word.substr(i+1,word.length()+1);
+                string sub = word.substr(0,i) + word.substr(i+1,word.length());
                 longest = max(longest,m[sub]+1);   
             }
             
