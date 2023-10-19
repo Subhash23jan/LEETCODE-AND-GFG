903685326eeaa88d920f39cc0f362150d6e3b432
Subhash D <subhash613d@gmail.com>
2023-10-19 08:55:24 +0530

    Time: 0 ms (100%), Space: 6.5 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,27 +1,49 @@
-class Solution {
-public:
-    bool backspaceCompare(string s, string t) {
-        stack<char>st1,st2;
-        for(auto it:s)
+class Solution
+{
+    public:
+        bool backspaceCompare(string s, string t)
         {
-            if(it=='#')
+            int i = s.size() - 1, j = t.size() - 1, cnts = 0, cntt = 0;
+            while (i >= 0 || j >= 0)
             {
-                if(st1.size())
-                 st1.pop();
-            }else{
-                st1.push(it);
+                char s_ch = '#';
+                while (i >= 0)
+                {
+                    if (s[i] == '#')
+                    {
+                        cnts++;
+                        i--;
+                        continue;
+                    }
+                    if (cnts == 0 )
+                    {
+                        s_ch = s[i];
+                        i--;
+                        break;
+                    }
+                    cnts--;
+                    i--;
+                }
+                char t_ch = '#';
+                while (j >= 0)
+                {
+                    if (t[j] == '#')
+                    {
+                        cntt++;
+                        j--;
+                        continue;
+                    }
+                    if (cntt == 0 )
+                    {
+                        t_ch = t[j];
+                        j--;
+                        break;
+                    }
+                    cntt--;
+                    j--;
+                }
+                if (s_ch != t_ch) return false;
             }
+            return true;
         }
-        for(auto it:t)
-        {
-            if(it=='#')
-            {
-                if(st2.size())
-                 st2.pop();
-            }else{
-                st2.push(it);
-            }
-        }
-        return st1==st2;
-    }
 };
