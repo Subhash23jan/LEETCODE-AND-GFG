cb68b453d91608614ff48f1a5386465c4e2c9573
Subhash D <subhash613d@gmail.com>
2024-06-06 14:06:41 +0530

    Time: 50 ms (82.91%) | Memory: 31.8 MB (29.12%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(std::vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+        std::unordered_map<int, int> count;
+        for (int card : hand) {
+            count[card]++;
+        }
+
+        std::vector<int> sortedKeys;
+        for (auto& pair : count) {
+            sortedKeys.push_back(pair.first);
+        }
+        std::sort(sortedKeys.begin(), sortedKeys.end());
+
+        for (int key : sortedKeys) {
+            if (count[key] > 0) { 
+                int startCount = count[key];
+                for (int i = key; i < key + groupSize; i++) {
+                    if (count[i] < startCount) {
+                        return false;
+                    }
+                    count[i] -= startCount;
+                }
+            }
+        }
+        return true;
+    }
+};
