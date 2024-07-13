dc6656e33e4c3edb7c636bce007497c373975028
Subhash D <subhash613d@gmail.com>
2024-07-13 22:48:14 +0530

    Time: 67 ms (17.67%) | Memory: 19.4 MB (69.64%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+    public:
+        vector<int> smallestRange(vector<vector < int>> &nums)
+        {
+            priority_queue<pair<int, pair<int, int>>> pq;
+            int n = nums.size();
+            int mx = INT_MIN;
+            for (int i = 0; i < n; i++)
+            {
+                pq.push({ -nums[i][0],
+                    {
+                        i,
+                        0
+                    } });
+                mx = max(mx, nums[i][0]);
+            }
+            int ans = INT_MAX;
+            vector<int> res(2, 0);
+            while (true)
+            {
+                int range = mx + pq.top().first;
+                int r = pq.top().second.first;
+                int c = pq.top().second.second;
+                if (ans > range)
+                {
+                    ans = range;
+                    res[0] = -pq.top().first;
+                    res[1] = mx;
+                }
+                pq.pop();
+                if (c < nums[r].size() - 1)
+                {
+                    pq.push({ -nums[r][c + 1],
+                        {
+                            r,
+                            c + 1
+                        } });
+                    mx = max(mx, nums[r][c + 1]);
+                    cout<<mx<<" "<<-pq.top().first<<endl;
+                }
+                if(pq.size()<n){
+                    break;
+                }
+            }
+            return res;
+        }
+};
