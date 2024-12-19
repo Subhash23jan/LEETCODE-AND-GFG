9fd98458b9c26a1dcb5d8aca8c58f1f5e65c5a65
Subhash D <subhash613d@gmail.com>
2024-12-19 18:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (7.32%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,15 +1,24 @@
-class Solution {
-public:
-    int maxChunksToSorted(vector<int>& arr) {
-        int num=0;
-        int ans=0;
-        for(int i=0;i<arr.size();i++)
+class Solution
+{
+    int solve(int i, vector<int> arr, int sum)
+    {
+        int n = arr.size();
+        if (i == n)
         {
-            num=(num|(1<<(arr[i])));
-            if(num==(1<<(i+1))-1){
-                ans++;
-            }
+            return 0;
         }
-        return ans;
+        int temp = (i *(i + 1)) / 2;
+        sum += arr[i];
+        if (sum == temp)
+        {
+            return 1 + solve(i + 1, arr, sum);
+        }
+        return solve(i + 1, arr, sum);
     }
+    public:
+        int maxChunksToSorted(vector<int> &arr)
+        {
+            int n = arr.size();
+            return solve(0, arr, 0);
+        }
 };
