79a3deef5ac7da4dda104dd4fbd9697795d75a5e
Subhash D <subhash613d@gmail.com>
2025-02-14 22:41:55 +0530

    Time: 39 ms (34.04%) | Memory: 85 MB (5.54%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,8 +1,8 @@
 class ProductOfNumbers
 {
     int cnt = 0;
-    unordered_map<int, int> mp;
-    vector<int> vec;
+    stack<int> st;
+    unordered_map < long long, int> mp;
     public:
         ProductOfNumbers()
         {
@@ -14,11 +14,7 @@ class ProductOfNumbers
         cnt++;
         if (num == 0)
         {
-            vec.push_back(cnt);
-        }
-        if (cnt == 1)
-        {
-            mp[cnt] = num;
+            st.push(cnt);
         }
         else
         {
@@ -35,17 +31,14 @@ class ProductOfNumbers
 
     int getProduct(int k)
     {
-        int ele = cnt - k;
-        int index = upper_bound(vec.begin(), vec.end(), ele) - vec.begin();
-        if (index < vec.size())
+        auto size = -k;
+        if (st.size())
         {
-            return 0;
+            size = st.top();
         }
-        if (mp[ele] == 0)
-        {
-            return mp[cnt];
-        }
-        return mp[cnt] / mp[ele];
+        if ((cnt - size) < k) return 0;
+        if (mp[cnt - k] == 0) return mp[cnt];
+        return mp[cnt] / mp[cnt - k];
     }
 };
 
